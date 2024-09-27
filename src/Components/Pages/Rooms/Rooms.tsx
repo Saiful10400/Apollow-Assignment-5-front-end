c221a046a092dca12ca8f17d97b5bde40befeaac
saiful10400 <saifulislam10400i5@gmail.com>
2024-09-28 04:06:20 +0600

    room card page added.

--- src/Components/Pages/Rooms/Rooms.tsx
+++ src/Components/Pages/Rooms/Rooms.tsx
@@ -1,11 +1,27 @@
-import React from 'react';
+
+import { useGetRoomsQuery } from "../../../Redux/api/api";
+import Loading from "../../SharedComponent/Loading";
+import RoomCart from "../../Ui/RoomCart";
+import CenterAlign from "../../Helper/CenterAlign";
 
 const Rooms = () => {
-    return (
-        <div className='h-[200vh]'>
-            
-        </div>
-    );
+  const { data, isLoading } = useGetRoomsQuery(null);
+
+  return (
+    <CenterAlign>
+      <div className="h-[200vh]">
+        {isLoading ? (
+          <Loading />
+        ) : (
+          <div className="grid lg:grid-cols-3 grid-cols-1 xl:grid-cols-4 gap-3 mt-4">
+            {data?.data?.map((item) => {
+              return <RoomCart key={item._id} data={item} />;
+            })}
+          </div>
+        )}
+      </div>
+    </CenterAlign>
+  );
 };
 
 export default Rooms;
